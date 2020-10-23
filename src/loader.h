e726a2d86037009d8a31d167e61ce84a05eebcb8
VMI452942\Administrator <kevinlopez151004@gmail.com>
2020-10-23 09:11:53 +0200

    Added Files

---
+++ src/loader.h
@@ -0,0 +1,6 @@
+void AddSC_npc_promotion();
+
+void AddPromotionScripts() 
+{
+    AddSC_npc_promotion();
+}
---
+++ src/mod_promotion.cpp
@@ -0,0 +1,158 @@
+/**
+    This plugin can be used for common player customizations
+ */
+
+#include "ScriptMgr.h"
+#include "Player.h"
+#include "Config.h"
+#include "Chat.h"
+#include "GossipDef.h"
+#include "ScriptedCreature.h"
+#include "ScriptedGossip.h"
+#include "Language.h"
+#include "DatabaseEnv.h"
+#include "WorldSession.h"
+
+class announce_module : public PlayerScript{
+public:
+
+    announce_module() : PlayerScript("announce_module") { }
+
+    void OnLogin(Player* player) override {
+        if (sConfigMgr->GetBoolDefault("announce_module.enableHelloWorld", true)) {
+            ChatHandler(player->GetSession()).SendSysMessage("Hello World from Promotion-Module!");
+        }
+    }
+};
+
+class npc_promotion : public CreatureScript
+{
+public:
+    npc_promotion() : CreatureScript("npc_promotion") { }
+
+    bool OnGossipHello(Player* player, Creature* creature) override
+    {
+        //ChatHandler handler();
+
+        if (player->getClass() == CLASS_DEATH_KNIGHT)
+        {
+            //player->SendPreparedGossip();
+            return true;
+        }
+
+        if (player->getLevel() != 1)
+        {
+            if (player && creature)
+                SendGossipMenuFor(player, 68, creature);
+                return true;
+        }
+
+        switch (player->getClass())
+        {
+        case CLASS_WARRIOR:
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Arms", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Protect", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Fury", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 3);
+            break;
+        case CLASS_WARLOCK:
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Demonology", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Destruction", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Affliction", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 3);
+            break;
+        case CLASS_DRUID:
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Tank", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Heal", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Balance", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 3);
+            break;
+        case CLASS_HUNTER:
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Marksmanship ", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Survival", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Beast Mastery", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 3);
+            break;
+        case CLASS_MAGE:
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Arcane", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Fire", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Frost", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 3);
+            break;
+        case CLASS_ROGUE:
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Assassination", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Combat", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Mutilate", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 3);
+            break;
+        case CLASS_PRIEST:
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Dicipline", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Holy", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Shadow", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 3);
+            break;
+        case CLASS_PALADIN:
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Holy", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Protection", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Retribution", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 3);
+            break;
+        case CLASS_SHAMAN:
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Elemental", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Restoration", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "PvP Enhancement", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 3);
+            break;
+        default:
+            break;
+        }
+
+        SendGossipMenuFor(player, 68, creature);
+
+        return true;
+    }
+
+    bool OnGossipSelect(Player* player, Creature* creature, uint32 /*sender*/, uint32 action) override
+    {
+        uint32 accountID = player->GetSession()->GetAccountId();
+        QueryResult result = CharacterDatabase.PQuery("SELECT COUNT(`guid`) FROM `characters` WHERE `account`=%u", accountID);
+        Field* fields = result->Fetch();
+        uint32 pjts = fields[0].GetUInt32();
+
+        if (pjts == 1)
+            if (action > GOSSIP_ACTION_INFO_DEF && action < 1007)
+                // Level
+                player->GiveLevel(80);
+
+                //Money 5k
+                player->ModifyMoney(50000000);
+
+       SendGossipMenuFor(player, 68, creature);
+
+       return true;
+
+    }
+
+    bool AccountPromotion(Player* player)
+    {
+        std::string ip_address;
+        QueryResult result = LoginDatabase.PQuery(
+            "SELECT last_ip FROM account WHERE id = %u",
+            player->GetSession()->GetAccountId());
+
+        if (result)
+        {
+            Field* fields = result->Fetch();
+            ip_address = fields[0].GetCString();
+
+            QueryResult result1 = CharacterDatabase.PQuery("SELECT `account` FROM `account_promotion` WHERE `account` = %u OR `ip` = '%s'", player->GetSession()->GetAccountId(), fields[0].GetCString());
+            if (result1)
+            {
+                Field* pField = result1->Fetch();
+                uint32 account = pField[0].GetUInt32();
+                if (account)
+                    return true;
+            }
+            return false;
+        }
+        return false;
+    }
+};
+
+
+void AddSC_npc_promotion() {
+    new announce_module();
+    new npc_promotion();
+}
+
